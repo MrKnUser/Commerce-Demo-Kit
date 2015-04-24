0327fad7ce90e2c8002024cdb3c7b2b5368724c6
sveinung@bvnetwork.no <sveinung@bvnetwork.no>
2015-04-24 14:54:27 +0200

    Started on a cart dropdown in header

---
+++ src/web/Scripts/app/shopping/HeaderCartController.js
@@ -0,0 +1,54 @@
+﻿/* jshint -W099 */
+/* global jQuery:false */
+/* global angular:false */
+(function ($, productApp, commercestarterkit) {
+
+    "use strict";
+
+    productApp.controller("HeaderCartController", ['$scope', 'handleCartService', '$sce', function ($scope, handleCartService, $sce) {
+
+       
+
+        $scope.GetCartItemsToHeader = function(language) {
+            console.log(language);
+            handleCartService.getCart(language, 'default').$promise.then(function (data) {
+                    getCartInfoFromResponse(data);
+               
+            },
+				function () {
+				    //console.log(response);
+				});
+
+        };
+        $scope.removeFromCart = function(product) {
+            console.log(product);
+            //$scope.trackRemoveFromCart(product);
+            handleCartService.remove($scope.language, product, 'default').$promise.then(function (data) {
+                getCartInfoFromResponse(data);
+                commercestarterkit.retrieveCartCounters();
+
+
+            }, function () {
+
+            });
+
+        };
+
+        function getCartInfoFromResponse(data) {
+            $scope.cartTotal = data.Total;
+            $scope.cartTotalAmount = data.TotalAmount;
+            $scope.cartTotalLineItemsAmount = data.TotalLineItemsAmount;
+            $scope.cartShipping = data.Shipping;
+            $scope.cartTax = data.Tax;
+            $scope.cartDiscount = data.Discount;
+            $scope.products = data.LineItems;
+            $scope.messages = data.Result;
+            $scope.discountCodes = data.DiscountCodes;
+        }
+
+      
+
+        
+    }]);
+
+})(jQuery, window.productApp, window.commercestarterkit);
