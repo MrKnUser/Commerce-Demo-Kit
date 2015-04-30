20a0b2d74f8b07bc0a43522b0d72bd7dec64bd7b
evest <steve@celius.cc>
2015-04-30 12:27:03 +0200

    Added facet displayname

--- src/web/Business/FacetRegistry/FacetDefinition.cs
+++ src/web/Business/FacetRegistry/FacetDefinition.cs
@@ -1,12 +1,26 @@
 using System.Collections.Generic;
 using EPiServer.Find;
 using EPiServer.Find.Api.Facets;
+using EPiServer.Framework.Localization;
 
 namespace OxxCommerceStarterKit.Web.Business.FacetRegistry
 {
     public abstract class FacetDefinition
     {
+        private string _displayName = null;
         public string Name { get; set; }
+        public string DisplayName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_displayName))
+                    return LocalizationService.Current.GetString("/facetregistry/" + Name.ToLowerInvariant(), Name);
+                return _displayName;
+            }
+
+            set { _displayName = value; }
+        }
+
         public string FieldName { get; set; }
         public string RenderType { get; set; }
 
