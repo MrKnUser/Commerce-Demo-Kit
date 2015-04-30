63f88e60e05ffd616c8262e37d3b40e4a609d99e
evest <steve@celius.cc>
2015-04-30 13:28:58 +0200

    Added ID to numeric ranges for easier selection

--- src/web/Business/FacetRegistry/SelectableNumericRange.cs
+++ src/web/Business/FacetRegistry/SelectableNumericRange.cs
@@ -4,6 +4,23 @@ namespace OxxCommerceStarterKit.Web.Business.FacetRegistry
 {
     public class SelectableNumericRange : NumericRange, ISelectable
     {
+        private string _id;
+
+        public string Id
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(_id))
+                {
+                    string from = From == null ? "MIN" : From.ToString();
+                    string to = To == null ? "MAX" : To.ToString();
+                    return from + "-" + to;
+                }
+                return _id;
+            }
+            set { _id = value; }
+        }
+
         public bool Selected { get; set; }
     }
 }
