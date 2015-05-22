257e3288491bedb83b389b442a06f9a52988fc65
evest <steve@celius.cc>
2015-05-22 10:32:15 +0200

    Fixed similar products slider on mobile

--- src/web/Scripts/app/shopping/SimilarProductsController.js
+++ src/web/Scripts/app/shopping/SimilarProductsController.js
@@ -34,7 +34,24 @@ function initSimilarProducts() {
         currentPagerPosition: 'left',
         autoWidth: false,
         pager: false,
-        adaptiveHeight:true
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    item: 3,
+                    slideMove: 1,
+                    slideMargin: 6,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    item: 2,
+                    slideMove: 1
+                }
+            }
+        ]
 
 });
     
