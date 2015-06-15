bd8ade07677e66ba78e808c17a734a14b9e322b8
evest <steve@celius.cc>
2015-06-15 21:21:58 +0200

    Fixed a bug in html scrubbing, missing null check

--- src/web/Helpers/HtmlHelpers.cs
+++ src/web/Helpers/HtmlHelpers.cs
@@ -556,6 +556,7 @@ namespace OxxCommerceStarterKit.Web.Helpers
         /// <returns></returns>
         public static string ScrubHtml(string value)
         {
+            if (value == null) return null;
             var step1 = Regex.Replace(value, @"<[^>]+>|&nbsp;", "").Trim();
             var step2 = Regex.Replace(step1, @"\s{2,}", " ");
             return step2;
