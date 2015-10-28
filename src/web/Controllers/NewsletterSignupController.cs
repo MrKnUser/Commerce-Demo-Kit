1dcaca72d1a6b66f0f298df2b8fab534fba5c398
evest <steve@celius.cc>
2015-10-28 14:29:03 +0100

    Added missing signup form for newsletter
    For some reason, this was not in the project. Added and upgraded markup
    to work with Bootstrap 3.

---
+++ src/web/Controllers/NewsletterSignupController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using OxxCommerceStarterKit.Web.Models.Blocks;
+
+namespace OxxCommerceStarterKit.Web.Controllers
+{
+    public class NewsletterSignupBlockController : BlockController<NewsletterSignupBlock>
+    {
+        public override ActionResult Index(NewsletterSignupBlock currentBlock)
+        {
+            return PartialView(currentBlock);
+        }
+    }
+}
