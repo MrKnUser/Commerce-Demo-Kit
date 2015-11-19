15c17ed6f3a31fe3ef6a843a14909908161d1675
Janaka Fernando <janaka.fernando@episerver.com>
2015-11-19 15:38:38 +0000

    Store location page

---
+++ src/web/Business/UIDescriptor/CoordinatesEditorDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+
+
+namespace OxxCommerceStarterKit.Web.Business.UIDescriptor
+{
+    [EditorDescriptorRegistration(TargetType = typeof(string), UIHint = UIHint)]
+    public class CoordinatesEditorDescriptor : EditorDescriptor
+    {
+        public const string UIHint = "CoordinatesEditorDescriptor";
+
+        public CoordinatesEditorDescriptor()
+        {
+            ClientEditingClass = "tedgustaf.googlemaps.Editor";
+        }
+    }
+}
